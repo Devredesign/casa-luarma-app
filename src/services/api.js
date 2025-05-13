8087a166e91da7adafe45209212e684830f76a1d
Devredesign <espacioaq@gmail.com>
2025-05-12 23:58:50 -0600

    lilo comentado 2

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
-  //baseURL: `${process.env.REACT_APP_API_BASE_URL}/api`
+  //baseURL: 'http://localhost:5000/api'
+  baseURL: `${process.env.REACT_APP_API_BASE_URL}/api`
 });
 
 export default api;
